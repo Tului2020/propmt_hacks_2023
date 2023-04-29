970c63036be23f9e95526b6eb70124385441dcc8
Tului Gantulga <tgantulga@ucdavis.edu>
2023-04-29 10:21:30 -0700

    minor changes

--- apps/hotline/src/main.tsx
+++ apps/hotline/src/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './app/app';
 
-import App from "./app/app";
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
 root.render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>
+  </HashRouter>
 );
