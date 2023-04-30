7aa44b277aeb605fe3dd4dd8d291f2685f3aeec0
Tului Gantulga <tgantulga@ucdavis.edu>
2023-04-30 01:46:50 -0700

    ui changes

--- ui_api/apps/hotline/src/app/views/Login.tsx
+++ ui_api/apps/hotline/src/app/views/Login.tsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 import { Button, Dialog, DialogContent, DialogTitle, TextField, Grid } from '@mui/material';
 import { useState } from 'react';
 import { setLocallyAuthed } from '../helpers/variables';
+import 'react-phone-number-input/style.css';
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input';
 
 interface Props {
   className?: string;
@@ -12,6 +14,8 @@ interface Props {
 const Login = (props: Props) => {
   const { className, setUsername, username } = props;
   const [_username, _setUsername] = useState('');
+  const [_phone, _setPhone] = useState<any>('');
+  const loginButtonDisabled = !(isValidPhoneNumber(_phone) && _username);
 
   return (
     <Dialog
@@ -23,18 +27,28 @@ const Login = (props: Props) => {
       </DialogTitle>
       <DialogContent>
         <Grid container>
-          <Grid item className='content' >
+          <Grid item className='content'>
             <TextField
               onChange={(e) => _setUsername(e.target.value)}
               size='small'
+              label='Name'
+              style={{ marginBottom: '10px' }}
+            />
+            <PhoneInput
+              placeholder='Enter phone number'
+              value={_phone}
+              onChange={_setPhone}
+              international={false}
+              defaultCountry='US'
             />
           </Grid>
           <Grid item className='content'>
             <Button
               onClick={() => {
                 setUsername(_username);
-                setLocallyAuthed(_username);
+                setLocallyAuthed(_username, _phone);
               }}
+              disabled={loginButtonDisabled}
             >
               Login
             </Button>
